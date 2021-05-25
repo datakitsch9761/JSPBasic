22889ba86ac4db72611495493a1cc4caac878a11
datakitsch9761 <data.kitsch@gmail.com>
2021-05-25 14:12:32 +0900

    케시제어, 기타등등

---
+++ joseph/SungJuk.java
@@ -0,0 +1,74 @@
+package joseph;
+
+import java.io.Serializable;
+
+public class SungJuk implements Serializable {
+	private String name;
+	private int kor;
+	private int mat;
+	private int eng;
+	private int tot;
+	private double avg;
+	private char grd;
+	
+	public SungJuk() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+	
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getTot() {
+		tot = kor + eng + mat;
+		return tot;
+	}
+
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+
+	public double getAvg() {
+		avg = (double)getTot()/3;
+		return avg;
+	}
+
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+
+	public char getGrd() {
+		return grd;
+	}
+
+	public void setGrd(char grd) {
+		this.grd = grd;
+	}
+}
